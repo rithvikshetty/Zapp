6cc6c51543b973def3d073ef066d200e6ec2c061
rithvikshetty <rithvikshetty99@gmail.com>
2021-11-28 19:43:20 +0530

    modified gateway

--- driver-services/src/main/java/com/zapp/driverservices/DriverServicesApplication.java
+++ driver-services/src/main/java/com/zapp/driverservices/DriverServicesApplication.java
@@ -1,10 +1,12 @@
 package com.zapp.driverservices;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan(basePackages = {"com.zapp"})
 public class DriverServicesApplication {
 
--- gateway-services/src/main/java/com/zapp/gatewayservices/GatewayServicesApplication.java
+++ gateway-services/src/main/java/com/zapp/gatewayservices/GatewayServicesApplication.java
@@ -2,8 +2,10 @@ package com.zapp.gatewayservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class GatewayServicesApplication {
 
 	public static void main(String[] args) {
